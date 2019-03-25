56c91809bc21a8bceac1a095140a59cee43121aa
Wayne Dela Cruz <wdelacruz@nurbality.com>
2019-03-25 12:59:49 +0800

    Refactor

--- src/routes/tripRoute.ts
+++ src/routes/tripRoute.ts
@@ -1,14 +1,13 @@
-import { Router } from "express";
-import * as body from "body-parser";
+import * as express from "express";
 import * as validator from "express-validator/check";
 import { tripFeedback } from "../handlers/tripFeedback";
 import { authentication } from "../handlers/authentication";
 
-export const tripRoute = Router();
+export const tripRoute = express.Router();
 
 tripRoute.post('/', [
   authentication.authentication,
-  body.json(),
+  express.json(),
   validator.body('userId').exists().isString(),
   validator.body('howItCompares').exists().isString(),
   validator.body('lightFlashesWokeUp').optional().isString(),
