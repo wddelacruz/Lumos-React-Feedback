7d0d16ad46c44e1350c67d49a19b36d09e6940a9
Wayne Dela Cruz <wdelacruz@nurbality.com>
2019-03-25 12:52:26 +0800

    Create post trip feedback handler

--- src/handlers/tripFeedback.ts
+++ src/handlers/tripFeedback.ts
@@ -1,6 +1,25 @@
 import { RequestHandler } from "express";
+import {
+  UserFeedbackModel,
+} from "../models";
 
 export namespace tripFeedback {
   export const post: RequestHandler = async (req, res, next) => {
+    try {
+      let user = await UserFeedbackModel.findByUserId(req.body.userId);
+      let trip = user.createTripFeedback();
+
+      trip.howItCompares = req.body.howItCompares;
+      trip.lightFlashesWokeUp = req.body.lightFlashesWokeUp;
+
+      await trip.save();
+
+      res.json(trip).once('finish', next);
+    } catch (err) {
+      res.status(500).json({
+        msg: `An error occured`,
+        err,
+      }).once('finish', next);
+    }
   }
 }
