e01a793cd9cbbdaab6331624dd2901987a64b220
Wayne Dela Cruz <wdelacruz@nurbality.com>
2019-03-25 10:01:17 +0800

    Create app entry point

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import * as express from 'express';
+import { routes } from './routes';
+
+const PORT = process.env.PORT || 8440;
+const app = express();
+
+const server = app.listen(PORT, () => {
+  console.log('App now listening on', PORT);
+});
+
