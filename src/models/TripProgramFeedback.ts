f3d6aea8eb947acb616f7afecf73e866a9aa5631
Wayne Dela Cruz <wdelacruz@nurbality.com>
2019-03-25 10:30:25 +0800

    Create enums

--- src/models/TripProgramFeedback.ts
+++ src/models/TripProgramFeedback.ts
@@ -7,5 +7,18 @@ export const TripProgramFeedbackModel = new TripProgramFeedback().getModelForCla
 
 export namespace TripProgramFeedback {
   export enum WhatBothersYouMost {
+    CANT_FALL_ASLEEP = 'i_cant_fall_asleep',
+    I_FEEL_SLEEPY = 'i_feel_sleepy_next_day',
+  }
+
+  export enum HowDoesItCompare {
+    BETTER = 1,
+    SAME = 0,
+    WORSE = -1,
+  }
+
+  export enum YesNo {
+    YES = 'YES',
+    NO = 'NO',
   }
 }
