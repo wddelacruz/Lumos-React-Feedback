781f19d4cbf3a534a810d9fabe75c03f334031b2
Wayne Dela Cruz <wdelacruz@nurbality.com>
2019-04-11 16:46:04 +0800

    Update index.ts

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,6 @@
 import * as mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost:27017/lumos-sleep-feedback');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/lumos-sleep-feedback');
 
 export * from './TripProgramFeedback';
 export * from './UserFeedback';
